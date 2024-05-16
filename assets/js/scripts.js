7cb8d0ee071493a16d2c9f7a360d2372a44ef0ea
quockiettuyetanh <phamgiabao48756@gmail.com>
2024-05-17 00:17:23 +0700

    a

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -23,3 +23,31 @@ var swiper = new Swiper(".swiper-container", {
 });
 
 new WOW().init();
+
+setTimeout(function () {
+  const loadingScreen = document.querySelector(".loading-screen");
+  loadingScreen.style.opacity = "0";
+  document.querySelector(".main-screen").style.opacity = "1";
+  setTimeout(function () {
+    loadingScreen.remove();
+    document.body.style.overflow = "auto";
+  });
+}, 3000);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.querySelector("#heart-fall");
+
+  function createHeart() {
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = Math.random() * 5 + 3 + "s";
+    container.appendChild(heart);
+
+    setTimeout(() => {
+      heart.remove();
+    }, parseFloat(heart.style.animationDuration) * 1000);
+  }
+
+  setInterval(createHeart, 2000);
+});
