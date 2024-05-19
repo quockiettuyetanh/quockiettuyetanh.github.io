75b50b6865f63f1f4b00dd7fe8284b9ea0da28d4
quockiettuyetanh <phamgiabao48756@gmail.com>
2024-05-19 22:13:39 +0700

    update firebase connect web

---
+++ assets/js/firebase.js
@@ -0,0 +1,28 @@
+// var firebaseConfig = {
+//   apiKey: "AIzaSyBpCguTPbaJ9_G6zpEi60CV3vrYZ44Mn2Q",
+//   authDomain: "quockiettuyetanh.firebaseapp.com",
+//   projectId: "quockiettuyetanh",
+//   storageBucket: "quockiettuyetanh.appspot.com",
+//   messagingSenderId: "910658884623",
+//   appId: "1:910658884623:web:6cdb8dab38bae9afd87f7a",
+// };
+// firebase.initializeApp(firebaseConfig);
+
+// var database = firebase.database();
+
+// function save() {
+//   var email = document.getElementById("email").value;
+//   var name = document.getElementById("name").value;
+//   var phone = document.getElementById("phone").value;
+//   var content = document.getElementById("content").value;
+
+//   database.ref("email/" + email).set({
+//     email: email,
+//     name: name,
+//     phone: phone,
+//     content: content,
+//   });
+
+//   alert("Đã lưu");
+//   console.log("Đã lưu");
+// }
